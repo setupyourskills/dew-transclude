10219faae9049ec06d1891852b47e7ae25f5ee37
Thubten Rigzin <thubtenrigzin@gmail.com>
2025-07-23 20:48:50 +0800

    feat: auto-remove embedded lines based on ':>' suffix if '!' is removed before the link

--- lua/neorg/modules/external/dew-transclude/module.lua
+++ lua/neorg/modules/external/dew-transclude/module.lua
@@ -10,53 +10,70 @@ module.load = function()
   module.private.set_autocmd()
 end
 
+module.config.public = {
+  block_end_marker = "==="
+}
+
 module.private = {
   set_autocmd = function()
-    autocmd({ "FileType" }, {
-      pattern = "norg",
+    autocmd({ "BufEnter", "CursorMoved" }, {
       callback = function()
-        local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+        if vim.bo.filetype == "norg" then
+          local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 
-        for i = #lines, 1, -1 do
-          local line = lines[i]
-          local title = string.match(line, "!%{:(.-):%}%[.-%]")
+          for i = #lines, 1, -1 do
+            local line = lines[i]
 
-          if title then
-            local file_name = neorg.modules.get_module("core.dirman.utils").expand_path(title)
-            local block_lines = {}
+            local disable_and_get_number = string.match(line, "%{:.-:%}%[.-%]:>%s(%d+)")
 
-            if line:find(":> ", 1, true) then
+            if disable_and_get_number and not string.match(line, "!%{") then
+              local new_line = line:gsub("%]:>%s%d+$", "]")
+
+              vim.api.nvim_buf_set_lines(0, i - 1, i, false, { new_line })
+              vim.api.nvim_buf_set_lines(0, i, i + disable_and_get_number, false, {})
+
               goto continue
             end
 
-            local content = modules.get_module("external.neorg-dew").read_file(file_name)
+            local title = string.match(line, "!%{:(.-):%}%[.-%]")
 
-            if content then
-              local inside_block = false
+            if title then
+              local file_name = neorg.modules.get_module("core.dirman.utils").expand_path(title)
+              local block_lines = {}
 
-              for _, line2 in ipairs(content) do
-                local is_match_title = line2:match "^%*%s"
-                if inside_block then
-                  local new_line = modules.get_module("external.neorg-dew").level_up(line2, 2)
-                  table.insert(block_lines, new_line)
-                end
-
-                if is_match_title then
-                  if inside_block then
-                    break
-                  end
-                  inside_block = true
-                elseif line2:match "===" then
-                  break
-                end
+              if line:find(":> ", 1, true) then
+                goto continue
               end
 
-              vim.api.nvim_buf_set_lines(0, i, i + 1, false, block_lines)
-            end
+              local content = modules.get_module("external.neorg-dew").read_file(file_name)
 
-            vim.api.nvim_buf_set_lines(0, i - 1, i, false, { line .. ":> " .. #block_lines })
+              if content then
+                local inside_block = false
+
+                for _, line2 in ipairs(content) do
+                  local is_match_title = line2:match "^%*%s"
+                  if inside_block then
+                    local new_line = modules.get_module("external.neorg-dew").level_up(line2)
+                    table.insert(block_lines, new_line)
+                  end
+
+                  if is_match_title then
+                    if inside_block then
+                      break
+                    end
+                    inside_block = true
+                  elseif line2:match(module.config.public.block_end_marker) then
+                    break
+                  end
+                end
+
+                vim.api.nvim_buf_set_lines(0, i, i + 1, false, block_lines)
+              end
+
+              vim.api.nvim_buf_set_lines(0, i - 1, i, false, { line .. ":> " .. #block_lines - 1 })
+            end
+            ::continue::
           end
-          ::continue::
         end
       end,
     })
