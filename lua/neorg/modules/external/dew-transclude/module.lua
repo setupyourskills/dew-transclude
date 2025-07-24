999032ec5f95780754cd13fa51b95bb2ed9a0f1c
Thubten Rigzin <thubtenrigzin@gmail.com>
2025-07-24 10:49:10 +0800

    feat: add enable/disable/toggle/refresh subcommands functionality

--- lua/neorg/modules/external/dew-transclude/module.lua
+++ lua/neorg/modules/external/dew-transclude/module.lua
@@ -36,15 +36,104 @@ module.config.public = {
 
 module.private = {
   enable = function()
+    local row_position, line = modules.get_module("external.neorg-dew").get_line_at_cursor_position()
+
+    if line:find("{", 1, true) and not line:find("!", 1, true) then
+      local new_line = line:gsub("%{", "!{")
+
+      vim.api.nvim_buf_set_lines(0, row_position - 1, row_position, false, { new_line })
+    end
   end,
 
   disable = function()
+    local row_position, line = modules.get_module("external.neorg-dew").get_line_at_cursor_position()
+
+    if line:find("!{", 1, true) then
+      local new_line = line:gsub("!%{", "{")
+
+      vim.api.nvim_buf_set_lines(0, row_position - 1, row_position, false, { new_line })
+    end
   end,
 
   toggle = function()
+    local _, line = modules.get_module("external.neorg-dew").get_line_at_cursor_position()
+
+    if module.private.is_enabled(line) then
+      module.private.disable()
+    else
+      module.private.enable()
+    end
   end,
 
   refresh = function()
+    module.private.disable()
+
+    vim.defer_fn(function()
+      module.private.enable()
+    end, 100)
+  end,
+
+  delete_inserted_lines = function(line, position, line_number)
+    local new_line = line:gsub("%]:>%s%d+$", "]")
+
+    vim.api.nvim_buf_set_lines(0, position - 1, position, false, { new_line })
+    vim.api.nvim_buf_set_lines(0, position, position + line_number, false, {})
+  end,
+
+  is_enabled = function(line)
+    return string.match(line, "!%{:(.-):%}%[.-%]")
+  end,
+
+  is_inserted = function(line)
+    if line:find(":> ", 1, true) then
+      return true
+    end
+  end,
+
+  transclusion_disabled = function(line, position)
+    local disable_and_get_number = string.match(line, "%{:.-:%}%[.-%]:>%s(%d+)")
+
+    if disable_and_get_number and not string.match(line, "!%{") then
+      module.private.delete_inserted_lines(line, position, disable_and_get_number)
+
+      return true
+    end
+  end,
+
+  embed_note = function(line, position, path)
+    local file_name = neorg.modules.get_module("core.dirman.utils").expand_path(path)
+    local block_lines = {}
+
+    if module.private.is_inserted(line) then
+      return
+    end
+
+    local content = modules.get_module("external.neorg-dew").read_file(file_name)
+
+    if content then
+      local inside_block = false
+
+      for _, line2 in ipairs(content) do
+        local is_match_title = line2:match "^%*%s"
+        if inside_block then
+          local new_line = modules.get_module("external.neorg-dew").level_up(line2)
+          table.insert(block_lines, new_line)
+        end
+
+        if is_match_title then
+          if inside_block then
+            break
+          end
+          inside_block = true
+        elseif line2:match(module.config.public.block_end_marker) then
+          break
+        end
+      end
+
+      vim.api.nvim_buf_set_lines(0, position, position + 1, false, block_lines)
+    end
+
+    vim.api.nvim_buf_set_lines(0, position - 1, position, false, { line .. ":> " .. #block_lines - 1 })
   end,
 
   set_autocmd = function()
@@ -56,55 +145,13 @@ module.private = {
           for i = #lines, 1, -1 do
             local line = lines[i]
 
-            local disable_and_get_number = string.match(line, "%{:.-:%}%[.-%]:>%s(%d+)")
+            local path = module.private.is_enabled(line)
 
-            if disable_and_get_number and not string.match(line, "!%{") then
-              local new_line = line:gsub("%]:>%s%d+$", "]")
-
-              vim.api.nvim_buf_set_lines(0, i - 1, i, false, { new_line })
-              vim.api.nvim_buf_set_lines(0, i, i + disable_and_get_number, false, {})
-
-              goto continue
+            if path then
+              module.private.embed_note(line, i, path)
+            else
+              module.private.transclusion_disabled(line, i)
             end
-
-            local title = string.match(line, "!%{:(.-):%}%[.-%]")
-
-            if title then
-              local file_name = neorg.modules.get_module("core.dirman.utils").expand_path(title)
-              local block_lines = {}
-
-              if line:find(":> ", 1, true) then
-                goto continue
-              end
-
-              local content = modules.get_module("external.neorg-dew").read_file(file_name)
-
-              if content then
-                local inside_block = false
-
-                for _, line2 in ipairs(content) do
-                  local is_match_title = line2:match "^%*%s"
-                  if inside_block then
-                    local new_line = modules.get_module("external.neorg-dew").level_up(line2)
-                    table.insert(block_lines, new_line)
-                  end
-
-                  if is_match_title then
-                    if inside_block then
-                      break
-                    end
-                    inside_block = true
-                  elseif line2:match(module.config.public.block_end_marker) then
-                    break
-                  end
-                end
-
-                vim.api.nvim_buf_set_lines(0, i, i + 1, false, block_lines)
-              end
-
-              vim.api.nvim_buf_set_lines(0, i - 1, i, false, { line .. ":> " .. #block_lines - 1 })
-            end
-            ::continue::
           end
         end
       end,
